57d4922e63fe21b6e3fbc5e46603d12c362ae222
Biplov Kumar <Kumarb@thepaycocld.onmicrosoft.com>
2023-08-29 20:14:39 +0530

    Added icon

--- App.tsx
+++ App.tsx
@@ -5,12 +5,14 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
+  Platform,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
+  PermissionsAndroid,
   Text,
   useColorScheme,
   View,
@@ -21,6 +23,7 @@ import {
   Header,
 } from 'react-native/Libraries/NewAppScreen';
 import Config from './src/utils/Config';
+import messaging from '@react-native-firebase/messaging';
 
 
 function App(): JSX.Element {
@@ -30,6 +33,103 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
   };
 
+  useEffect(() => {
+      // The screen is focused
+      callApi()
+  }, []);
+
+  useEffect(() => {
+    //Only For Android
+    const initial = async () => {
+      // Assume a message-notification contains a "type" property in the data payload of the screen to open
+      
+      try {
+        await messaging().registerDeviceForRemoteMessages();
+      } catch (error) {
+        console.log('registerDeviceForRemoteMessages Errror: ', error);
+      }
+
+      messaging().setBackgroundMessageHandler(async i =>
+        console.log(i, 'BACKGROUND MESSAGE'),
+      );
+      messaging().onMessage(async rm => {
+        let msg = rm?.notification;
+        if (msg && msg?.body) {
+          console.log('=================onMessage===================');
+          console.log(rm);
+          console.log('====================================');
+          // showNoti(msg?.title, msg?.body);
+        }
+      });
+
+      messaging().onNotificationOpenedApp(rm => {
+        let msg = rm?.notification;
+        if (msg && msg?.body) {
+          console.log('=================onNotificationOpenedApp===================');
+          console.log(rm);
+          console.log('====================================');
+        }
+      });
+      // Check whether an initial notification is available
+      messaging()
+        .getInitialNotification()
+        .then(rm => {
+          let msg = rm?.notification;
+          if (msg && msg?.body) {
+            console.log('=============getInitialNotification=======================');
+            console.log(rm);
+            console.log('====================================');
+          }
+        });
+    };
+
+    initial();
+  }, []);
+
+  const callApi = async () => {
+    if (Platform.OS ===  'ios') {
+      requestUserPermission();
+    } else {
+      try {
+        if (Number(Platform.Version) >= 33) {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+          );
+
+          await getToken();
+        }
+      } catch (error) {
+        console.log('POST_NOTIFICATIONS error', error);
+      }
+    }
+  }
+
+
+  async function requestUserPermission() {
+    try {
+      const authStatus = await messaging().requestPermission();
+      await getToken();
+      // console.log('AuthStatus:', authStatus)
+      // const enabled =
+      //   authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      //   authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    } catch (error) {
+      console.log('messaging requestPermission', error);
+    }
+  }
+
+  const getToken = async () => {
+    try {
+      const token = await messaging().getToken();
+     console.log('====================================');
+     console.log(token);
+     console.log('====================================');
+      // onPressSubmit();
+    } catch (error) {
+      console.log(error);
+      // onPressSubmit();
+    }
+  };
 
   return (
     <SafeAreaView style={backgroundStyle}>
--- index.js
+++ index.js
@@ -5,5 +5,13 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+
+//get data in Quit state.
+messaging().setBackgroundMessageHandler(async (rm) => {
+    let msg = rm?.notification
+    // showNoti(msg?.title, msg?.body)
+    console.log('Message handled in the background!',rm);
+});
 
 AppRegistry.registerComponent(appName, () => App);
